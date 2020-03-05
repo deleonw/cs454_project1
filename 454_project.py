4fa4ed8a17a002194c7e0d68aef3202693af5140
Alex <barajasr@sonoma.edu>
2020-03-04 22:16:48 -0800

    finished problem 1

--- 454_project.py
+++ 454_project.py
@@ -23,96 +23,6 @@ def isAccepted(p):
         return True
     return False
 
-def recursiveNumAccepted(size, language, states, holder):
-    # NFA SetUp
-    # Dictionary
-    #   {
-    #   State # : { 'a' : state #, 'b' : state #, 'c' : state #, 'd' : state #},
-    #   State # : { 'aa' : state #, 'ab' : state #, 'ac' : state #, 'ad' : state #},
-    #   ...
-    #
-    #   }
-    nfa = dict()
-
-    # change variable below later
-    for i in range(0, size):
-        accepted = listOfAccepted(i+1, language, states, holder)
-        temp = dict()
-        for j in range(0, len(accepted)):
-            temp[accepted[j]] = calcPos(accepted[j])
-        nfa[i] = temp
-    print(str(nfa))
-    print(len(nfa))
-
-def listOfAccepted(size, language, states, holder):
-    # Iterative version of finding a list of accepted states at given SIZE
-    # Returns the list of accepted string
-    # Takes a while to calculate for any size 13 and greater
-    for n in range(1, size+1):
-        if n < 6:
-            holder.clear()
-            for i in states:
-                holder.append(i)
-            states.clear()
-            for string in holder:
-                for character in language:
-                    temp = string + character
-                    states.append(temp)
-
-                    #if n == size:
-                        #count += 1
-            #holder.clear()
-      
-        elif n == 6:
-            holder.clear()
-            for i in states:
-                holder.append(i)
-            states.clear()
-            for string in holder:
-                temp = string[:]
-                for character in language:
-                    temp = temp + character
-                    if isAccepted(temp):
-                        #if n == size:
-                            #count += 1
-                        states.append(temp)
-                    temp = temp[0:len(temp) - 1]
-            # holder.clear()
-        else:
-            holder.clear()
-            for i in states:
-                holder.append(i)
-            states.clear()
-            for string in holder:
-                temp = string[1:]
-                for character in language:
-                    temp = temp + character
-                    if isAccepted(temp):
-                        #if n == size:
-                            #count += 1
-                        states.append(temp)
-                    temp = temp[0:len(temp) - 1]
-            # holder.clear()
-        holder.clear()
-
-    #print(str(states))
-    #print(len(states))
-    #print(count)
-    return states
-
-# Temporary
-def recursiveNumAccepted(size, language,count, states, holder):
-    # NFA SetUp
-    # Dictionary
-    #   {
-    #   State # : { 'a' : state #, 'b' : state #, 'c' : state #, 'd' : state #},
-    #   State # : { 'a' : state #, 'b' : state #, 'c' : state #, 'd' : state #},
-    #   ...
-    #
-    #   }
-    states = dict()
-
-
 def calcPos(string):
     # Given a string of language {'a', 'b', 'c', 'd'} it calculates the an integer
     # which will be the State number.
@@ -133,13 +43,14 @@ def calcPos(string):
 
 def possiblestring():
     allstring = []
+    allstring.append("")
     for i in range(1, 6):
         for perm in itertools.product("abcd", repeat=i):
             temp = ""
             allstring.append(temp.join(perm))
     return allstring
 
-def nextstate(string, prev, curr):
+def nextstate(key, string, prev, curr):
     count = 0
     tempA = string + 'a'
     tempB = string + 'b'
@@ -166,46 +77,26 @@ def nextstate(string, prev, curr):
             tempD = tempD[1:]
         count += prev[calcPos(tempD)]
 
-    curr[calcPos(string)] = count
+    curr[key] = count
 
 
 def main():
-    # size = int(input("Size of string (Enter 0 to quit): "))
-    # while size != 0:
-    #     language = ['a', 'b', 'c', 'd']
-    #     states = ['']
-    #     holder = []
-    #     answer = listOfAccepted(size, language, states, holder)
-    #     # prints list of accepted strings
-    #     print(answer)
-    #     # prints the number of accepted strings
-    #     print(len(answer))
-    #     size = int(input("\nSize of string (Enter 0 to quit): "))
-
-        prev = [1] * 1365
+    size = int(input("Size of string (Enter 0 to quit): "))
+    prev = [1] * 1365
+    curr = [0] * 1365
+    base4 = {}
+    possible = possiblestring()
+    for element in possible:
+        temp = calcPos(element)
+        base4[temp] = element
+    for i in range(size):
+        for k, v in base4.items():  # go through prev list
+            nextstate(k, v, prev, curr)
+        prev = curr
         curr = [0] * 1365
+    print(prev[0])
 
-        base4 = {}
-        possible = possiblestring()
-        for element in possible:
-            temp = calcPos(element)
-            base4[temp] = element
 
-        n = 1
-        for i in range(7):
-            for k, v in base4.items():  # go through prev list
-                nextstate(v, prev, curr)
-            prev = curr
-            curr = [0] * 1365
-        for ele in prev:
-            print(ele)
 main()
 
-#
-#
-# prev = [1] * 1365
-# curr = [0] * 1365
 
-
-
-# def nextstate(index, prev, curr):
