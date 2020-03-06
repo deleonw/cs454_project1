a3df5add092656ad96ac9ea2d5cbf3638aed822a
delinton14 <mr.delintonw@gmail.com>
2020-03-05 18:16:01 -0800

    merge

--- prob2.py
+++ prob2.py
@@ -20,32 +20,47 @@ def Findstring(k, d):
     queue = [] # initialize a queue
     parent = [None] * (k+1)
     label = [None] * (k+1)
-    visited = [0] * (k+1) # array of visited states, all false
-    visited[k] = 1
+    visited = [False] * (k+1) # array of visited states, all false
+    visited[k] = True
     queue.append(k) # added the the last element to the queue
     while len(queue) > 0: # queue is not empty could also say while True ?
-        curr = queue[0]
-        queue.pop(0)
+        curr = queue.pop(0)
         for elem in d:
             next = delta(curr, elem, k)
             if next == 0:
                 break
-            elif visited[next] == 0: # state is false or not visted
-                visited[next] = 1
+            elif visited[next] == False: # state is false or not visted
+                visited[next] = True
                 parent[next] = curr
                 label[next] = elem
                 queue.append(next)
+    for i in parent:
+        if i == 26147:
+            print('found')
+    print(parent)
+    print(label)
     if next != 0:
             print("No solution")
     else:
         trace(parent, label)
 
-def trace(parent, label):
-    pass
+def trace(parent, label, k):
+    parentValue = parent[0]
+    solution = str(label[0])
+    while(parentValue != k):
+        solution += str(label[parentValue])
+        parentValue = parent[parentValue]
+    return solution
 
 def main():
-    k = 26147
-    d = [1, 3]
-    Findstring(k, d)
+    #k = 26147
+    #d = [1, 3]
+    #Findstring(k, d)
+
+    parent = [4, 3, 1, 5, 2]
+    label = [1, 4, 3 , 5, 2]
+    k = 5
+    print(trace(parent, label, k))
+
 
 main()
